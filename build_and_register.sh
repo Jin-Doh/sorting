44ef593d3d6a4cc063a1c434171ddc760b2e72aa
KyungHo Kim <qqaa3030@gmail.com>
2025-07-04 16:49:36 +0900

    docs: change the language to english

--- build_and_register.sh
+++ build_and_register.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-# 빌드할 바이너리 이름
+# The name of the binary to be built
 BIN_NAME="sorting"
 
-# 현재 디렉토리에서 main.go 빌드
+# Build the main.go in the current directory
 go build -o "$BIN_NAME" main.go
 
-# 바이너리를 /usr/local/bin 으로 이동 (sudo 필요할 수 있음)
+# Move the binary to /usr/local/bin (may require sudo)
 if [ -w /usr/local/bin ]; then
     mv -f "$BIN_NAME" /usr/local/bin/
 else
     sudo mv -f "$BIN_NAME" /usr/local/bin/
 fi
 
-echo "설치 완료: /usr/local/bin/$BIN_NAME"
-echo "터미널에서 '$BIN_NAME' 명령어로 실행할 수 있습니다."
+echo "Installation complete: /usr/local/bin/$BIN_NAME"
+echo "You can run it using the command '$BIN_NAME' in the terminal."
